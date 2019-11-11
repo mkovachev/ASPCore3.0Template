bbcfd11fda13b955297e94c55f4fe15f7ddc7488
mkovachev <mkovachev6@gmail.com>
2019-11-11 14:44:04 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
